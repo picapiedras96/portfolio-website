42a88998fb8848461d555210db32d0d142b84e65
picapiedras96 <118584987+picapiedras96@users.noreply.github.com>
2024-05-02 01:07:28 +0000

    Added additional links, styling, dynamic data and API route.

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)  #instance of Flask object
 
+# sample database using list
+PROJECTS = [
+    {
+        'id': 1,
+        'title': 'Personal Website',
+        'description': '    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    }
+]
+
 
 @app.route("/")  #route/ sub address
-def hello_world():
+def home():
   return render_template("home.html")
+
+@app.route("/api/projectlistobjects")
+def list_projects():
+  return jsonify(PROJECTS)
+  
+@app.route("/projects")
+def projects():
+  return render_template("projects.html", projects=PROJECTS)
+
+@app.route("/contact")
+def contact():
+  return render_template("contact.html")
 
 
 if __name__ == "__main__":  #if this file is run directly perform action
