75a9be7681f86f2086e6781e84234a98ceb13cdb
picapiedras96 <118584987+picapiedras96@users.noreply.github.com>
2024-07-17 02:03:55 +0000

    updated content and font size on contact and projects pages.

--- app.py
+++ app.py
@@ -3,26 +3,30 @@ from flask import Flask, render_template, jsonify
 app = Flask(__name__)  #instance of Flask object
 
 # sample database using list
-PROJECTS = [
-    {
-        'id': 1,
-        'title': 'Personal Website',
-        'description': '    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    }
-]
+PROJECTS = [{
+    'id':
+    1,
+    'title':
+    'pedrothedev.com',
+    'description':
+    '   The website you are currently browsing on is my first official launched project. I decided to create this website in May 2024 as an opportuinity to apply and display my development skills. I decided to implement the website with flask as it was an opportunity to use Python on my project one of my favorite languages. The domain "pedrothedev.com" came into existance with the idea to title my page "Vote for Pedro", sadly this domain was already taken and not worth the price. In this site you will find various implementations of CSS and HTML as well as some Python. My end goal for this site is to create a presense as I grow and develop more projects, this site is a continous project and will have future updates as I test new ideas.'
+}]
 
 
 @app.route("/")  #route/ sub address
 def home():
   return render_template("home.html")
 
+
 @app.route("/api/projectlistobjects")
 def list_projects():
   return jsonify(PROJECTS)
-  
+
+
 @app.route("/projects")
 def projects():
   return render_template("projects.html", projects=PROJECTS)
+
 
 @app.route("/contact")
 def contact():
